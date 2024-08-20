f255ca8238faecd906f8057fa9950483a6213933
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-08-20 17:11:02 -0300

    formatações

--- formata.py
+++
@@ -1,6 +0,0 @@
-def desenha_jogador(screen, posicao):
-    pygame.draw.rect(screen, BRANCO, (*posicao, 50, 50))
-
-def atualiza_pontuacao(pontuacao):
-    texto = fonte.render(f"Pontuação: {pontuacao}", True, BRANCO)
-    screen.blit(texto, (10, 10))
