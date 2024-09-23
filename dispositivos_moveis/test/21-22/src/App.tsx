3cb8db8592a5d1a5e09fdb26cfa87a11ac2811ef
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-09-23 20:51:33 -0300

    resolução de exercícios

--- dispositivos_moveis/test/21-22/src/App.tsx
+++ dispositivos_moveis/test/21-22/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Saudacao(props: any){
+  return <h1>Olá, {props.nome}!</h1>
+}
+
+function Contador(){
+  const [contador, setContador] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Você clicou {contador} vezes</p>
+      <button onClick={() => setContador(contador + 1)}>Clique aqui</button>
     </div>
-  );
+  )
+}
+
+function App() {
+  return <Contador />
 }
 
 export default App;
